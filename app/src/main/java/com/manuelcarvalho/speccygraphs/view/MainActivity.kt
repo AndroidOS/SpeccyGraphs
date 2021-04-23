07892c040ce33f9ad9b51be0212022a917eec41b
AndroidOS <burrowsgit@gmail.com>
2021-04-24 08:02:36 +1000

    Added get permission (1)

--- app/src/main/java/com/manuelcarvalho/speccygraphs/view/MainActivity.kt
+++ app/src/main/java/com/manuelcarvalho/speccygraphs/view/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.manuelcarvalho.speccygraphs.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
@@ -10,6 +12,8 @@ import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.manuelcarvalho.speccygraphs.R
@@ -30,11 +34,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fragImage: ImageView
     private val fileName = "image.asm"
 
+    private val STORAGE_PERMISSION_CODE = 101
+    private val CAMERA_PERMISSION_CODE = 105
+    private val PHOTO_PERMISSION_CODE = 106
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //var view = ScreenCanvas(this)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        checkPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                STORAGE_PERMISSION_CODE
+        )
+
+        checkPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                STORAGE_PERMISSION_CODE
+        )
 
         viewModel = ViewModelProviders.of(this)[AppViewModel::class.java]
 
@@ -53,6 +71,8 @@ class MainActivity : AppCompatActivity() {
             //val bart2 = resize(resources.getDrawable(R.drawable.bart))
             viewModel.newImage.value = bart2
         }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -93,5 +113,25 @@ class MainActivity : AppCompatActivity() {
         val b = (image as BitmapDrawable).bitmap
         val bitmapResized = Bitmap.createScaledBitmap(b, 256, 192, false)
         return bitmapResized
+    }
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity, permission)
+                == PackageManager.PERMISSION_DENIED
+        ) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(
+                    this@MainActivity, arrayOf(permission),
+                    requestCode
+            )
+        } else {
+            Toast.makeText(
+                    this@MainActivity,
+                    "Permission already granted",
+                    Toast.LENGTH_SHORT
+            )
+                    .show()
+        }
     }
 }
